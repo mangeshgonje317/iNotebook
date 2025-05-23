a181aaeeef51ac56b1ec589e99bd376ab2f835c9
Your Nckkfclsme <you@example.com>
2025-05-23 22:35:17 +0530

    Your commit message here

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/inotebook";
+// const mongoURI = "mongodb+srv://mangeshgonje12:SPSiT3IcMTd0vxBY@cluster0.47o5wcv.mongodb.net/";
+
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+         console.log("Connected to MongoDB successfully")    
+};
+
+module.exports = connectToMongo;
