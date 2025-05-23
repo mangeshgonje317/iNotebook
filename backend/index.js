fdffeb2c79951c495aa9cd293fe8e97bcd47e143
mangeshgonje317 <73571444+mangeshgonje317@users.noreply.github.com>
2025-05-23 23:11:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,15 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.aap],
+            methods:["POST","GET"],
+            credentials:true
+  }
+))
 app.use(express.json());
+mongoose.connect('mongodb+srv://mangeshgonje12:Mangesh@01@cluster0.47o5wcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 // available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/Notes',require('./routes/Notes'))
