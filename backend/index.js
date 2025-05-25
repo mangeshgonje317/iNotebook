b4bc5dab09f105cde00ef19ce36a5e5cdb129f33
mangeshgonje317 <73571444+mangeshgonje317@users.noreply.github.com>
2025-05-25 13:32:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,9 @@ app.use(cors(
 app.use(express.json());
 mongoose.connect('mongodb+srv://mangeshgonje12:Mangesh@01@cluster0.47o5wcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 // available routes
+app.get("/", (req, res) => {
+  res.send("welcome to my server");
+});
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/Notes',require('./routes/Notes'))
 
