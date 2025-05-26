0b66747ea352608fce7f475afa9581f40c802c47
mangeshgonje317 <73571444+mangeshgonje317@users.noreply.github.com>
2025-05-26 15:50:00 +0530

    Update Login.js
    changed local host

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,7 @@ const[credentials, setCredentials]= useState({email:" ",password:""})
     const handelSubmit=async (e)=>{
         e.preventDefault();
         
-         const response =await fetch("http://localhost:5000/api/auth/login",{
+         const response =await fetch("https://inotebook-backend-85i7.onrender.com/api/auth/login",{
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
