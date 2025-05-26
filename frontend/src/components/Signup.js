55a8e363428ab8535bb5d2c8d08468d65b3c3b29
mangeshgonje317 <73571444+mangeshgonje317@users.noreply.github.com>
2025-05-26 15:50:30 +0530

    Update Signup.js
    changed local host

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -9,7 +9,7 @@ function Signup(props) {
   const handelSubmit=async (e)=>{
         e.preventDefault();
         const {name ,email,password,cpassword} = credentials;
-         const response =await fetch("http://localhost:5000/api/auth/createuser",{
+         const response =await fetch("https://inotebook-backend-85i7.onrender.com/api/auth/createuser",{
           
         method: 'POST',
         headers: {
